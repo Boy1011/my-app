c7ebbdabb00cd0a34f65773f798f4fb037d9ddd1
Boy1011 <boyo_1011@hotmail.com>
2022-12-30 12:18:15 +0700

    add amplify

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,27 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter()
-	}
+///** @type {import('@sveltejs/kit').Config} */
+//const config = {
+//	kit: {
+//		adapter: adapter()
+//	}
+//};
+
+//export default config;
+
+//aws adapter
+import adapter from "@sveltejs/adapter-static";
+
+export default {
+  kit: {
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: "build",
+      assets: "build",
+      fallback: null,
+      precompress: false,
+      strict: true,
+    }),
+  },
 };
-
-export default config;
